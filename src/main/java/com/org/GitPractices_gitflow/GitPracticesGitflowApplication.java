0e2976ba2c976e7fe549e57695fb5b65f471c437
GovindS2025 <govindjsg19@gmail.com>
2025-10-03 10:29:17 +0100

    first commit from dev

---
+++ src/main/java/com/org/GitPractices_gitflow/GitPracticesGitflowApplication.java
@@ -0,0 +1,16 @@
+package com.org.GitPractices_gitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticesGitflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPracticesGitflowApplication.class, args);
+
+        //testing 
+
+	}
+
+}
---
+++ src/test/java/com/org/GitPractices_gitflow/GitPracticesGitflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.GitPractices_gitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticesGitflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
